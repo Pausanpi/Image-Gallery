7affdebe943be359a9d3a44a04cde7ab87659ab0
Pausanpi <pausanpi1606@gmail.com>
2024-11-19 21:44:12 +0100

    update

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const CLIENT_ID = 'wstcP1uva1xCFUniI_FQcFzqBywFysFbiSiQiF6IkkQ';
 const CLIENT_SECRET = 'GEatidYddFc6SE4Rnv5_3lwIcYnb2MajmeuK_RrQvjY';
 //const REDIRECT_URI = 'http://localhost:5500/src/layout.html';
-const REDIRECT_URI = '/src/layout.html'
+const REDIRECT_URI = 'src/layout.html'
 const AUTH_URL = `https://unsplash.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=public`;
 
 // Botón de inicio de sesión
@@ -33,7 +33,7 @@ if (authCode) {
             localStorage.setItem('accessToken', accessToken);
             alert('Login successful!');
             //window.location.href = 'http://localhost:5500/src/layout.html'; // Redirigir a la página layout.html
-            window.location.href = '/src/layout.html'; 
+            window.location.href = 'src/layout.html'; 
         } else {
 			alert('Error: No access token received');
 			console.error('No access token received:', data);
